ab7baed8278a274f8cb0a7254e65b1d7eeee92d9
aman <amankrsingh.2012@gmail.com>
2025-01-25 01:21:35 +0530

    Default and Manual Constructor

--- src/main/java/org/scaler/IntroToOops/Client.java
+++ src/main/java/org/scaler/IntroToOops/Client.java
@@ -2,21 +2,34 @@ package org.scaler.IntroToOops;
 
 public class Client {
     public static void main(String[] args) {
-        Student st = new Student();
-        st.age = 35;
-
-//        Student st2 = st;
-//        st2.age+=10;
+//        Student st = new Student();
+//        st.age = 35;
 //
+////        Student st2 = st;
+////        st2.age+=10;
+////
+////        System.out.println(st.age);
+////        System.out.println(st2.age);
+//
+////        st.setName("Akshay");
+////        System.out.println(st.getName());
+//
+//        System.out.println(st.batch);
+////        System.out.println(st.name);
 //        System.out.println(st.age);
-//        System.out.println(st2.age);
+//        System.out.println(st.psp);
 
-//        st.setName("Akshay");
-//        System.out.println(st.getName());
+        // Constructors
+//        Student st = new Student();
+//        Student st2 = new Student("Aman",26,100,"Best Batch");
+//        System.out.println(st.age);
+//        System.out.println(st2);
 
-        System.out.println(st.batch);
-//        System.out.println(st.name);
-        System.out.println(st.age);
-        System.out.println(st.psp);
+        Student st = new Student(); // 773
+        st.setName("SRK");
+
+        Student s2 = new Student(); // 774
+        s2.getName();
+        s2.setName("SRK");
     }
 }
--- src/main/java/org/scaler/IntroToOops/Student.java
+++ src/main/java/org/scaler/IntroToOops/Student.java
@@ -6,6 +6,31 @@ public class Student {
     protected double psp;
     public String batch;
 
+
+//   1 Student st = new Student();
+//   2  Student st2 = new Student("Aman",26,100,"Best Batch");
+
+    // Constructor is a special function
+//    1. Name as Class name
+//    2. no return type
+//    Initialise data members
+//    public Student(){
+//        name = "Abhilash";
+//        age = 20;
+//        psp = 30;
+//        batch = "xyz";
+//
+//    }
+
+
+//    public Student(String name2, int a, double b, String c){
+//        name = name2;
+//        age = a;
+//        psp = b;
+//        batch = c;
+//
+//    }
+
     void print(){
         System.out.println("Name"+ name);
     }
@@ -15,6 +40,6 @@ public class Student {
     }
 
     String getName(){
-        return name;
+        return this.name;
     }
 }
